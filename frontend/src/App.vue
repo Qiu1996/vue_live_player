27ea3a4b02e385a89456c615fe799b94298cac97
Qiu1996 <betty52332001@gmail.com>
2025-11-09 18:17:09 +0800

    feat: 建立 Vue 直播播放器基礎架構

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import '@mux/mux-player';
+const PLAYBACK_ID = 'EYZg25NXzdyuYn00nmHAoe64PbdrlPGtgoiMqIsi5km4';
+</script>
+
+<template>
+  <mux-player
+    :playback-id="PLAYBACK_ID"
+  />
+</template>
+
+<style scoped>
+</style>
