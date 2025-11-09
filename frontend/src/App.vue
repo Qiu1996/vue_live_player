1b5e9407c481dfeb2d20111dff07c4006e9e0ee9
Qiu1996 <betty52332001@gmail.com>
2025-11-09 22:27:11 +0800

    feat(fe): 實作前端串接/create_stream 端點，並顯示回傳的參數及串流網址

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,35 @@
 <script setup>
 import '@mux/mux-player';
-const PLAYBACK_ID = 'EYZg25NXzdyuYn00nmHAoe64PbdrlPGtgoiMqIsi5km4';
+import { ref } from 'vue';
+
+
+const PLAYBACK_ID = ref(null);
+const STREAM_KEY = ref(null);
+const STREAM_URL = ref("https://player.mux.com/");
+
+async function postapi(){
+  try{
+    const res = await fetch("http://127.0.0.1:8000/create_stream", {
+      method: 'POST'
+    });
+    const date = await res.json();
+    PLAYBACK_ID.value = date.playback_id;
+    STREAM_KEY.value = date.stream_key;
+
+  }catch(e){
+    console.log('err: ', e);
+  }
+
+}
+
 </script>
 
 <template>
-  <mux-player
-    :playback-id="PLAYBACK_ID"
-  />
+<button @click="postapi">Click</button>
+<div v-if="PLAYBACK_ID">
+  <p>STREAM_KEY: {{STREAM_KEY}}</p>
+  <a :href="STREAM_URL + PLAYBACK_ID">串流網址</a>
+</div>
 </template>
 
 <style scoped>
